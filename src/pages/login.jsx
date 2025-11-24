16c5cda420276a5348f633ae363abb9c90f39199
huzaifa910 <huzaifa2533@gmail.com>
2025-11-24 17:39:12 +0500

    resolve

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase";
-import "./Auth.css";
+import "./auth.css";
 import Swal from "sweetalert2";
 
 const Login = () => {
